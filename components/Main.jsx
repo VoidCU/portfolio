c20e9cb291bd4f5db30801fc90cee9a089bd9387
VoidCU <sarojprasadmainali@gmail.com>
2023-01-26 21:12:25 +0545

    Use &#39; instead of " ' "

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,10 +13,10 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            LET'S Build something together
+            LET&#39;S Build something together
           </p>
           <h1 className="py-4 text-gray-700">
-            Hi, I'm <span className="text-[#5651e5]">Saroj</span>
+            Hi, I&#39;m <span className="text-[#5651e5]">Saroj</span>
           </h1>
           <h1 className="py-2 text-gray-700">AI Learner and Explorer.</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
