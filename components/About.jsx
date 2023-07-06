68ddd5502c59889e62afc699f41f3757a7f92d29
Saroj Prasad Mainali <98652711+VoidCU@users.noreply.github.com>
2023-07-06 17:52:47 +0545

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -10,16 +10,7 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-300 tracking-widest text-lg">
-            Hello! I am Saroj, a computer engineering student at Thapathali
-            Campus. I am passionate about technology and always strive to create
-            innovative solutions through my work. My experience in game
-            development, app development, website development, and machine
-            learning has equipped me with a diverse set of skills that I can
-            bring to any project. I am dedicated to my craft and take pride in
-            delivering top-quality results. Whether you need a new app or
-            website, or have ideas for a game or machine learning project, I am
-            confident in my ability to bring your vision to life with
-            exceptional results.
+           Hello! I am Saroj, a computer engineering graduate from Thapathali Campus. I am passionate about technology and always strive to create innovative solutions through my work. With a strong foundation in game development, app development, website development, and machine learning, I possess a diverse set of skills that can be applied to any project. I am dedicated to my craft and take pride in delivering top-quality results. Whether you need a new app or website, or have ideas for a game or machine learning project, I am confident in my ability to bring your vision to life with exceptional results.
           </p>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-black rounded-full flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
