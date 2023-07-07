72c2713227c25c52396876e77c579c19314952ae
Saroj Prasad Mainali <98652711+VoidCU@users.noreply.github.com>
2023-07-07 09:06:50 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Saroj | Learner</title>
-        <meta name="description" content=" Delve into the world of Saroj, a talented computer engineering student at Thapathali Campus, renowned for their passion and innovative spirit in technology. With expertise in game development, app development, website development, and machine learning, Saroj brings a versatile skill set to every project. Experience their commitment to excellence and unrivaled craftsmanship as they transform ideas into reality, whether it's crafting captivating apps, websites, games, or exploring the realms of machine learning. Discover the boundless potential of technology through Saroj's ingenuity and unlock a future of limitless possibilities." />
-        <meta name="keywords" content="Saroj, VoidCU, Saroj Prasad Mainali, Vloe, Saroj Void, Void Saroj, computer engineering, technology, innovation, game development, app development, website development, machine learning, craftsmanship, excellence, future, possibilities" />
+        <title>Saroj Prasad Mainali | VoidCU</title>
+        <meta name="description" content=" Delve into the world of Saroj" />
+        <meta name="keywords" content="Saroj, VoidCU, Saroj Prasad Mainali, Vloe, Saroj Void, Void Saroj, computer engineering" />
       </Head>
       <Navbar />
       <Main />
