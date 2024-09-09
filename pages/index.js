d5ff6e49f31441e87bb2be314bc38d437f059b1c
Saroj Prasad Mainali <98652711+VoidCU@users.noreply.github.com>
2024-09-09 23:16:50 +0545

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <title>Saroj Prasad Mainali | VoidCU</title>
         <meta name="description" content=" Delve into the world of Saroj" />
         <meta name="keywords" content="Saroj, VoidCU, Saroj Prasad Mainali, Vloe, Saroj Void, Void Saroj, computer engineering" />
+        <meta name="ahrefs-site-verification" content="e5ae4aeef9d7b6709b0c1b71229ebfd56a332b4430f2139d6f4fff1584adc3f1">
       </Head>
       <Navbar />
       <Main />
